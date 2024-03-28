d90112d18a8bfdc9a66221cdc5a1324322f0603e
Atomic2ds <109296824+Atomic2ds@users.noreply.github.com>
2024-03-29 00:01:10 +0800

    Removed the glossary fromt the navigation menu (for now)

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ navigation = """
  <ul class="nav">
     <li><a href="/">Home</a></li>
      <li><a href="/blog">Blog</a></li>
-     <li><a href="/glossary">Glossary</a></li>
+     <li><a href="/projects">Projects</a></li>
      <li><a href="/about">About</a></li>
  </ul>
 </nav>
