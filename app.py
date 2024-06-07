70dcb814fc065433b658210a55f45c37a2685797
Atomic2ds <109296824+Atomic2ds@users.noreply.github.com>
2024-06-08 02:06:08 +0800

    Updated the port used in testing environments

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def unprocessable_entity(error):
 
 # Running the app (Development)
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True,port=80)
+    app.run(host="0.0.0.0",debug=True,port=8080)
